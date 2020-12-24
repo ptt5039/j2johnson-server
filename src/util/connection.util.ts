024368fd3361db1d2d4ec34fb1101532f9afe820
SSCWebs <sscwebservices@gmail.com>
2020-12-23 22:58:56 -0500

    perfect

--- src/util/connection.util.ts
+++ src/util/connection.util.ts
@@ -3,7 +3,7 @@ import { Pool } from "pg";
 const connectionConfiguration = {
    user: process.env.JTJ_DB_USERNAME,
    host: process.env.JTJ_DB_URL || 'localhost',
-   database: process.env.JTJ_DB_NAME || 'j2johnson',
+   database: process.env.JTJ_DB_NAME || 'postgres',
    password: process.env.JTJ_DB_PASSWORD,
    port: +process.env.JTJ_DB_PORT || 5432,
    max: 5
