239165eff12f9eead14c3b345dd12eeea0d5c0f1
SSCWebs <sscwebservices@gmail.com>
2020-12-28 22:00:27 -0500

    removed local host

--- src/util/connection.util.ts
+++ src/util/connection.util.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 const connectionConfiguration = {
    user: process.env.JTJ_DB_USERNAME,
-   host: process.env.JTJ_DB_URL || 'localhost',
+   host: process.env.JTJ_DB_URL,
    database: process.env.JTJ_DB_NAME || 'postgres',
    password: process.env.JTJ_DB_PASSWORD,
    port: +process.env.JTJ_DB_PORT || 5432,
