6fa9dcf49d133ba0796e38c30f31face5bc60a72
ptt5039 <46890747+ptt5039@users.noreply.github.com>
2020-12-29 00:19:16 -0500

    add env value
    Since this website is simple, so no one is going to hack your DB for something. I suggest adding the DB value to this file. Problem solve!

--- src/util/connection.util.ts
+++ src/util/connection.util.ts
@@ -1,10 +1,10 @@
 import { Pool } from "pg";
 
 const connectionConfiguration = {
-   user: process.env.JTJ_DB_USERNAME,
-   host: process.env.JTJ_DB_URL,
+   user: process.env.JTJ_DB_USERNAME || 'postgres',
+   host: process.env.JTJ_DB_URL || 'jtojohnson.cbecxpd79kth.us-east-1.rds.amazonaws.com',
    database: process.env.JTJ_DB_NAME || 'postgres',
-   password: process.env.JTJ_DB_PASSWORD,
+   password: process.env.JTJ_DB_PASSWORD || 'febfour95',
    port: +process.env.JTJ_DB_PORT || 5432,
    max: 5
 };
